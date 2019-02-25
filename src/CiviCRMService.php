3f41e018526972e082d8005cb3e768e2c0a01347
Klaas Eikelboom <klaas.eikelboom@civicoop.org>
2019-02-25 17:13:47 +0100

    Generated a skeleton service

---
+++ src/CiviCRMService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\emn_civisync;
+
+/**
+ * Class CiviCRMService.
+ */
+class CiviCRMService {
+
+  /**
+   * Constructs a new CiviCRMService object.
+   */
+  public function __construct() {
+
+  }
+
+}
